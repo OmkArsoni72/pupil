cd42077c073f1910c970da705c8a6ee6afa694eb
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-09-23 19:00:39 +0530

    refactor(code moved): codebase organized

--- mongoTest.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://founders:wuhqHr5sAoBW32DL@cluster0.5ahszmy.mongodb.net/Pupil_teach"
-client = MongoClient(uri)
-db = client["Pupil_teach"]
-print("Using database:", db.name)
-print("Available collections:", db.list_collection_names())
-grades = list(db["lesson_script"].find())
-print("Fetched grades from MongoDB:", grades)
